fb526978fab83cc3ee6dd18de344b09a4924ee15
ervanyuliawanputra-sketch <ervanyuliawanputra@gmail.com>
2026-02-20 02:41:11 +0700

    Update app.py
    ah udah bosen daritadi update mulu

--- app.py
+++ app.py
@@ -138,5 +138,8 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Render butuh port dinamis, kalau di lokal default ke 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
